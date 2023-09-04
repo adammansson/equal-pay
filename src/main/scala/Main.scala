2ce121302cc6e6fa2e88917f40d17d101733da02
Adam Månsson <adam.s.mansson@gmail.com>
2023-09-04 20:00:00 +0200

    first commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,36 @@
+import java.util.UUID
+
+case class Transaction(amount: Double, from: String, to: String):
+  override def toString: String = 
+    s"$from -> $to: $amount"
+
+case class Participant(name: String, balance: Double)
+
+def getTransactions(xs: Vector[Participant]): Vector[Transaction] =
+  xs match
+    case Vector() => Vector()
+    case _ =>
+      val average = xs.foldLeft(0.0)((acc, p) => acc + p.balance) / xs.length
+      val averageRemoved = xs.map(p => Participant(p.name, p.balance - average))
+      getTransactionsRecursive(averageRemoved)
+
+def getTransactionsRecursive(xs: Vector[Participant]): Vector[Transaction] =
+  xs.sortBy(_.balance) match
+    case Vector(first, last) => Vector(Transaction(last.balance, first.name, last.name))
+    case first +: middle :+ last =>
+      val difference = first.balance + last.balance
+      if difference < 0 then
+        Transaction(last.balance, first.name, last.name) +: getTransactionsRecursive(Participant(first.name, difference) +: middle)
+      else
+        Transaction(-first.balance, first.name, last.name) +: getTransactionsRecursive(middle :+ Participant(last.name, difference))
+    case _ => Vector()
+
+def participantsFromInput: Vector[Participant] =
+  io.StdIn.readLine() match
+    case "" => Vector()
+    case input => 
+      val splitInput = input.split(" ")
+      Participant(splitInput(0), splitInput(1).toDouble) +: participantsFromInput
+
+@main def run: Unit = 
+  getTransactions(participantsFromInput).foreach(println)
